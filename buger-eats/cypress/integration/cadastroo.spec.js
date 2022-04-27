58a9e33232c378c8a4087e2aadd574383ca8def9
thiagoluppi <thiago.luppi@icloud.com>
2022-04-27 12:30:43 +0100

    aula validando a busca de CEP

--- buger-eats/cypress/integration/cadastroo.spec.js
+++ buger-eats/cypress/integration/cadastroo.spec.js
@@ -30,7 +30,7 @@ describe('Cadastro', () => {
         rua: 'Rua Joaquim Floriano',
         numero: '1000',
         complemento: 'Ap 142',
-        bairro: 'Itain Bibi',
+        bairro: 'Itaim Bibi',
         cidade_uf: 'São Paulo/SP'
       }
     }
@@ -45,5 +45,9 @@ describe('Cadastro', () => {
 
     cy.get('input[name="address-number"]').type(entregador.endereco.numero)
     cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+    cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
+    cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
+    cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
   })
 })
